97fe303b8d63759d071dd373e92ed058f4c0e01d
Boris Bronovitsky <bronovitski@gmail.com>
2023-07-09 23:08:01 +0200

    add main GUI window class

---
+++ src/main/java/org/example/gui/GameField.java
@@ -0,0 +1,16 @@
+package org.example.gui;
+
+import javax.swing.*;
+
+public class GameField extends JFrame {
+
+    public GameField() {
+//        super("Swing Snake app");
+//
+//        System.out.println("JFrame thread name: " + Thread.currentThread().getName()
+//                + "; is the event dispatch thread? " + (SwingUtilities.isEventDispatchThread() ? "Yes" : "No"));
+
+        setVisible(true);
+
+    }
+}
